ac99ad23cf412756047cf395c2328cfb1f08627c
Fazal2307-dev <mdfazal2307@gmail.com>
2025-05-19 20:53:50 +0530

    added

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -3,19 +3,20 @@ import React from "react";
 
 const Square =(props)=>{
   return (
-  <div 
-   style={{
-    border:"1px solid",
-    height:"100px",
-    width:"40%",
-    display:"flex",
-    justifyContent:"center",
-    alignItems:"center"
-  
-  }}
-  className="square">
-    <h5>{props.Value}</h5>
-  </div>
+        <div 
+        onClick={()=> props.onClick}
+        style={{
+          border:"1px solid",
+          height:"100px",
+          width:"40%",
+          display:"flex",
+          justifyContent:"center",
+          alignItems:"center"
+        
+        }}
+        className="square">
+          <h5>{props.Value}</h5>
+        </div>
  )
 }
 export default Square;
